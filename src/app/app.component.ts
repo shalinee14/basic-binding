19ed2cc9d3be6b7a59c390ba30debd70d024d1a5
shalinee14 <shalinee14rathod@gmail.com>
2020-01-15 19:22:43 +0530

    4 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,16 @@ export class AppComponent  {
   month = ['Jan','Feb', 'Mar', 'Apr', 'May' , 'Jun' , 'Jul' , 'Aug' , 'Sep' , 'Oct' , 'Nov', 'Dec'];
 
   isAvailable = "false";
+
+  myFunction($event){
+    // alert('Button working');
+    console.log('Function working');
+    this.isAvailable = !this.isAvailable;
+  }
+  
+  changeMonth(event){
+    console.log('Month is change');
+    alert('Month is changed from the dropdown');
+    console.log(event);
+  }
 }
